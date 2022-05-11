b072537a5cae5a5b888012e4d83a4b2c0a5e0e83
Joy <parkerjn90@gmail.com>
2022-05-10 20:25:13 -0400

    import express.json middleware, 'save' function from database index, and getReposByUsername from helpers github. Also began writing post request

--- server/index.js
+++ server/index.js
@@ -1,13 +1,28 @@
+const { color, modifier, log } = require('console-log-colors');
+const { red, green, yellow, blue, magenta, cyan, bold, italic, underline } = color;
+
+const { save } = require('../database/index.js');
+const { getReposByUsername } = ('../helpers/github.js');
+
 const express = require('express');
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.json());
 
 app.post('/repos', function (req, res) {
   // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
+  getReposByUsername(req.body)
+    .then(list => {
+      save(list.data)
+      res.send(list.data);
+    }).catch(err => {
+      res.sendStatus(404);
+    });
   // save the repo information in the database
+
 });
 
 app.get('/repos', function (req, res) {
@@ -17,7 +32,7 @@ app.get('/repos', function (req, res) {
 
 let port = 1128;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
 
